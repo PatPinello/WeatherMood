d0e82065336fcaf1405e70b0f8d101626e749242
MrSpankledorf <patrickpinello@gmail.com>
2022-09-06 14:08:04 -0400

    Added Weather, Location Functions
    DeletUser, UpdateLoc, UpdateWeather, GetCurrentLoc, added weather and temperature to User Class

--- WeatherMood.py
+++ WeatherMood.py
@@ -2,33 +2,56 @@ import requests
 import sqlite3
 import googlemaps
 
-
-#Getting Users Location
-gmaps = googlemaps.Client(key='AIzaSyBF-KtkTNuBPSPLKRaar5Y_Wg55UO8tUG0')
-latLng = gmaps.geolocate()
-coord = latLng["location"]
-lat = coord["lat"]
-lng = coord["lng"]
-
-googleResponse = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s, %s&key=AIzaSyBF-KtkTNuBPSPLKRaar5Y_Wg55UO8tUG0" % (lat, lng))
-#print(googleResponse.status_code) #Check if api responds
-googleDict = googleResponse.json()
-
-#Gets users city
-city = googleDict['results'][0]['address_components'][2]["long_name"] 
-
-weather = requests.get("http://api.weatherapi.com/v1/current.json?key=ce694d555ff84cbbb3e122529220509&q=" + city)
-
-#Database
-con = sqlite3.connect("user.db")
+#Establishing Connection to Database
+con = sqlite3.connect("C:\\Users\patri\\Desktop\\WeatherMood\\WeatherMood\\user.db")
 cur = con.cursor()
 
-#Updates location of user
-def UpdateLoc(user, currentCity):
-    user.loc = currentCity
-    cur.execute("Update user set location = location where id = %s" % user.id)
+#Updates location of user to current location
+def UpdateLoc(user, city):
+    cur.execute("UPDATE user SET location = REPLACE(location, '%s','%s') WHERE id=%s" % (user.loc, city, user.id))
     con.commit()
-    return print(user.loc)
+    user.loc = city
+    return print(user.name + "'s location has been changed to '" + user.loc + "'")
+
+def UpdateWeather(user):
+
+    weatherData = requests.get("http://api.weatherapi.com/v1/current.json?key=ce694d555ff84cbbb3e122529220509&q=" + user.loc)
+    weatherDict = weatherData.json()
+    newWeather = weatherDict['current']['condition']['text']
+    newTemp = weatherDict['current']['temp_f']
+    print(newWeather)
+    cur.execute("UPDATE user SET weather = REPLACE(weather, '%s','%s') WHERE id=%s" % (user.weather, newWeather, user.id))
+    con.commit()
+    cur.execute("UPDATE user SET temp = REPLACE(temp, '%s','%s') WHERE id=%s" % (user.temp, newTemp, user.id))
+    con.commit()
+    
+    user.weather = newWeather
+    user.temp = newTemp
+
+    return print(user.name + "'s weather and temmperature has been changed to '" + user.weather + "'")
+
+#Get users current location
+def GetCurrentLoc():
+    
+    gmaps = googlemaps.Client(key='AIzaSyBF-KtkTNuBPSPLKRaar5Y_Wg55UO8tUG0')
+    latLng = gmaps.geolocate()
+    coord = latLng["location"]
+    lat = coord["lat"]
+    lng = coord["lng"]
+
+    googleResponse = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s, %s&key=AIzaSyBF-KtkTNuBPSPLKRaar5Y_Wg55UO8tUG0" % (lat, lng))
+    #print(googleResponse.status_code) #Check if api responds
+    googleDict = googleResponse.json()
+    #Gets users city
+    currentCity = googleDict['results'][0]['address_components'][2]["long_name"] 
+    return currentCity
+
+#Delete user from database
+def DeleteUser(user):
+    delete = "DELETE FROM user WHERE id=?"
+    cur.execute(delete, (user.id,))
+    con.commit()
+    return print("User '" + user.name + "' has been deleted")
 
 #Adds User to Database
 def AddUser(user):
@@ -36,8 +59,9 @@ def AddUser(user):
     if cur.fetchone():
         print("*** User already exists ***")
     else:
-        cur.execute("INSERT INTO user VALUES (?, ?, ?)", (user.name, user.loc, user.id))
+        cur.execute("INSERT INTO user VALUES (?, ?, ?, ?, ?)", (user.name, user.loc, user.weather, user.temp, user.id))
         con.commit() #Close transaction
+        print("New User '" + user.name + "' has been added")
 
 #Prints Database
 def PrintDB(database):
@@ -46,26 +70,37 @@ def PrintDB(database):
 
 #Defining User Class
 class User:
-    def __init__(self, name, loc, id):
+    def __init__(self, name, loc, id, weather = "None", temp = 70):
         self.name = name
         self.loc = loc
         self.id = id
+        self.weather = weather
+        self.temp = temp
 
-### Creating Table ###
-try:
-    cur.execute("CREATE TABLE user(name, location, id)")
-    print("Table created")
-except:
-    print("Existing table used")
+def main():
+    ### Creating Table ###
+    try:
+        cur.execute("CREATE TABLE user(name, location, weather, temp, id)")
+        print("Table created")
+    except:
+        print("Existing table used")
 
-dict = weather.json()
+    Pat = User("Pat", "Bergen", 0)
+    Austin = User("Austin", "Rochester", 1)
+    Katrina = User("Katrina", "Medellin", 2)
+    Ben = User("Ben", "Medellin", 3)
 
-Pat = User("Pat", "Mahopac", 0)
-Austin = User("Austin", "Rochester", 1)
-Katrina = User("Katrina", "Medellin", 2)
-Ben = User("Ben", "Medellin", 3)
+    AddUser(Katrina)
+    AddUser(Ben)
+    AddUser(Austin)
+    AddUser(Pat)
 
-AddUser(Katrina)
-UpdateLoc(Ben, 'Rome')
-PrintDB("user")
-#print(Ben.loc)
+    UpdateLoc(Ben, 'Bangkok')
+    UpdateLoc(Pat, GetCurrentLoc())
+    UpdateWeather(Pat)
+
+    #DeleteUser(Ben)
+    PrintDB("user")
+
+if __name__ == "__main__":
+    main()
