b3378c9e3d9b9a5994ac0a1c2dd03307cbcb950e
MrSpankledorf <patrickpinello@gmail.com>
2022-09-05 20:50:01 -0400

    Added Google API
    Gets users current location.
    Need to update location in database

--- WeatherMood.py
+++ WeatherMood.py
@@ -1,58 +1,71 @@
 import requests
 import sqlite3
+import googlemaps
 
-response = requests.get("http://api.weatherapi.com/v1/current.json?key=ce694d555ff84cbbb3e122529220509&q=Rochester")
-#print(response.status_code) #Check response
+
+#Getting Users Location
+gmaps = googlemaps.Client(key='AIzaSyBF-KtkTNuBPSPLKRaar5Y_Wg55UO8tUG0')
+latLng = gmaps.geolocate()
+coord = latLng["location"]
+lat = coord["lat"]
+lng = coord["lng"]
+
+googleResponse = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s, %s&key=AIzaSyBF-KtkTNuBPSPLKRaar5Y_Wg55UO8tUG0" % (lat, lng))
+#print(googleResponse.status_code) #Check if api responds
+googleDict = googleResponse.json()
+
+#Gets users city
+city = googleDict['results'][0]['address_components'][2]["long_name"] 
+
+weather = requests.get("http://api.weatherapi.com/v1/current.json?key=ce694d555ff84cbbb3e122529220509&q=" + city)
+
+#Database
 con = sqlite3.connect("user.db")
-cursor = con.cursor()
+cur = con.cursor()
 
-def UpdateLoc(user, name):
-    user.loc = name
+#Updates location of user
+def UpdateLoc(user, currentCity):
+    user.loc = currentCity
+    cur.execute("Update user set location = location where id = %s" % user.id)
+    con.commit()
     return print(user.loc)
 
-def InsertDB(user):
-    cursor.execute("INSERT INTO user VALUES (?, ?, ?)", (user.name, user.loc, user.id))
-    return print("User inserted")
+#Adds User to Database
+def AddUser(user):
+    cur.execute("SELECT 1 FROM user WHERE id=%s" % user.id) #Open transaction
+    if cur.fetchone():
+        print("*** User already exists ***")
+    else:
+        cur.execute("INSERT INTO user VALUES (?, ?, ?)", (user.name, user.loc, user.id))
+        con.commit() #Close transaction
 
+#Prints Database
 def PrintDB(database):
-    result = cursor.execute("SELECT * FROM " + database)
+    result = cur.execute("SELECT * FROM " + database)
     return print(result.fetchall())
 
+#Defining User Class
 class User:
     def __init__(self, name, loc, id):
         self.name = name
         self.loc = loc
         self.id = id
 
+### Creating Table ###
 try:
-    cursor.execute("CREATE TABLE user(name, location, id)")
+    cur.execute("CREATE TABLE user(name, location, id)")
     print("Table created")
 except:
     print("Existing table used")
 
-#Open transaction
-cursor.execute("SELECT 1 FROM user WHERE id=0")
-if cursor.fetchone():
-    print("User already exists")
-else:
-
-    cursor.execute("""
-        INSERT INTO user VALUES
-            ("Pat", "Mahopac", 0),
-            ("Austin", "Rochester", 1)
-                """)
-    #Close transaction
-    con.commit()
-
-dict = response.json()
-loc = dict["location"]
-name = loc["name"]
+dict = weather.json()
 
 Pat = User("Pat", "Mahopac", 0)
 Austin = User("Austin", "Rochester", 1)
 Katrina = User("Katrina", "Medellin", 2)
+Ben = User("Ben", "Medellin", 3)
 
-InsertDB(Katrina)
+AddUser(Katrina)
+UpdateLoc(Ben, 'Rome')
 PrintDB("user")
-
-#UpdateLoc(Pat, name)
+#print(Ben.loc)
